966aae02044fee9cdd9d6e1fae3a70072279e12e
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2017-01-25 18:19:43 -0500

    Initial commit, for real.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # d7
 A DrupalGap 8 module that provides a bridge to a Drupal 7 site via Services and Views Datasource.
+
+# Examples
+
+Provide a route that to deliver a node to a controller for rendering...
+
+*The route*
+```
+routes["example.article"] = {
+  "path": "/article\/(.*)",
+  "defaults": {
+    "_title": "Article",
+    _controller: d7.entityViewController,
+    _dgEntityType: 'node',
+    _dgController: example.pageArticle
+  }
+};
+```
+*The controller*
+```
+example.pageArticle = function(node) {
+
+  dg.setPageTitle(node.title);
+  
+  var content = {};
+  content.title = {
+    _markup: '<h1>' + node.title + '</h1>'
+  };
+  return content;
+  
+};
+```
---
+++ d7.js
@@ -0,0 +1,157 @@
+var d7 = new dg.Module(); // Create the module.
+dg.modules.d7 = d7; // Attach it to DrupalGap.
+
+/**
+ * Initializes the jDrupal JSON object.
+ */
+d7.init = function() {
+  // General properties.
+  //d7.csrf_token = false;
+  //d7.sessid = null;
+  //d7.connected = false; // Will be equal to true after the system connect.
+  d7.settings = {
+    sitePath: null,
+    basePath: '/'
+  };
+};
+
+// Init d7.
+d7.init();
+
+/**
+ * CONFIG
+ */
+d7.config = function(name) {
+  var value = typeof arguments[1] !== 'undefined' ? arguments[1] : null;
+  if (value) {
+    d7.settings[name] = value;
+    return;
+  }
+  return d7.settings[name];
+};
+
+d7.sitePath = function() {
+  return d7.settings.sitePath;
+};
+d7.basePath = function() {
+  return d7.settings.basePath;
+};
+d7.restPath = function() {
+  return this.sitePath() + this.basePath();
+};
+d7.path = function() {
+  return this.restPath().substr(this.restPath().indexOf('://')+3).replace('localhost', '');
+};
+
+/**
+ * VIEWS
+ */
+
+/**
+ * The Views constructor.
+ * @param {String} path The path to the Views REST Export in Drupal.
+ * @constructor
+ */
+d7.Views = function(path) {
+  this.path = path;
+  this.results = null;
+};
+
+/**
+ * Returns the path to the rest export.
+ * @returns {String}
+ */
+d7.Views.prototype.getPath = function() {
+  return this.path;
+};
+
+/**
+ * Returns the results, if any.
+ * @returns {*}
+ */
+d7.Views.prototype.getResults = function() {
+  return this.results;
+};
+
+/**
+ * Retrieves the Views' results from the Drupal site's rest export.
+ */
+d7.Views.prototype.getView = function() {
+  var self = this;
+  return new Promise(function(resolve, reject) {
+    var req = new XMLHttpRequest();
+    req.dg = {
+      service: 'views',
+      resource: null
+    };
+    req.open('GET', d7.restPath() + self.getPath());
+    var loaded = function() {
+      self.results = [];
+      var results = JSON.parse(req.response);
+      if (results.view.count) {
+        for (var i in results[results.view.root]) {
+          if (!results[results.view.root].hasOwnProperty(i)) { continue; }
+          self.results.push(results[results.view.root][i][results.view.child]);
+        }
+      }
+      resolve();
+    };
+    req.onload = function() {
+      if (req.status == 200) {
+        var invoke = jDrupal.moduleInvokeAll('rest_post_process', req);
+        if (!invoke) { loaded(); }
+        else { invoke.then(loaded); }
+      }
+      else { reject(Error(req.statusText)); }
+    };
+    req.onerror = function() { reject(Error("Network Error")); };
+    req.send();
+  });
+};
+
+/**
+ * Loads a view and fetches its results from the Drupal site.
+ * @param {String} path
+ */
+d7.viewsLoad = function(path) {
+  return new Promise(function(resolve, reject) {
+    var view = new d7.Views(path);
+    view.getView().then(function() {
+      resolve(view);
+    });
+  });
+};
+
+d7.showFormErrors = function(msg) {
+  var messages = [];
+  var data = JSON.parse(msg);
+  if (data.form_errors) {
+    for (var name in data.form_errors) {
+      if (!data.form_errors.hasOwnProperty(name)) { continue; }
+      messages.push(data.form_errors[name]);
+    }
+  }
+  else { messages.push(msg); }
+  if (messages.length) { dg.alert(messages.join('<br />')); }
+};
+
+d7.entityViewController = function(entityId) {
+  var content = {};
+  content.entity = {
+    _theme: 'bucket',
+    _grab: function() {
+      return new Promise(function (fill, dump) {
+        var route = dg.router.getActiveRoute();
+        entity_load(route.defaults._dgEntityType, entityId, {
+          success: function(entity) {
+            fill(route.defaults._dgController(entity));
+          },
+          error: function(xhr, status, msg) {
+            fill(route.defaults._dgController(null, xhr, status, msg));
+          }
+        });
+      });
+    }
+  };
+  return content;
+};
