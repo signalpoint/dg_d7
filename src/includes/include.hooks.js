09a58f64cc1da7b432aff66cd089e6f3c276cf2a
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-07-07 13:58:19 -0400

    Check for a connection before doing system connect, and abstract out error handler.

--- src/includes/include.hooks.js
+++ src/includes/include.hooks.js
@@ -10,27 +10,29 @@ function dg_d7_deviceready() {
   // Take over the start of the application.
   return new Promise(function(ok, err) {
 
-    system_connect({
-      success: function(data) {
-
-        // Tell DrupalGap to continue and resolve.
-        dg.continue();
+    // Set up a default handler that looks to see if anyone implemented hook_deviceready_offline() to handle the
+    // situation, otherwise it'll forward along to dg.error().
+    var errorHandler = function(xhr, status, msg) {
+      console.log(arguments);
+      var hookName = 'deviceready_offline';
+      if (!jDrupal.moduleImplements(hookName)) {
         ok();
-
-      },
-      error: function(xhr, status, msg) {
-        console.log(arguments);
-
-        // If no one implemented deviceready_offline to handle this situation, just call dg.error() to handle it.
-        var hookName = 'deviceready_offline';
-        if (!jDrupal.moduleImplements(hookName)) {
-          ok();
-          setTimeout(function() { dg.error(xhr, status, msg); }, 1);
-        }
-        else { jDrupal.moduleInvokeAll(hookName, xhr, status, msg); }
-
+        setTimeout(function() { dg.error(xhr, status, msg); }, 1);
       }
-    });
+      else { jDrupal.moduleInvokeAll(hookName, xhr, status, msg); }
+    };
+
+    // If the device has a connection, make the system connect call to d7, otherwise call the error handler.
+    if (dg.hasConnection()) {
+      system_connect({
+        success: function(data) {
+          dg.continue(); // Tell DrupalGap to continue and resolve.
+          ok();
+        },
+        error: errorHandler
+      });
+    }
+    else { errorHandler(null, null, dg.t('No network connection.')); }
 
   });
 }
